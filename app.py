e3042b31980fc55fddb1b7423e00c4a02c79e499
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 21:46:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from config import APP_TITLE
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    layout="wide"
+)
+
+st.title(APP_TITLE)
+st.markdown(" **Production-Grade FMCG Business Intelligence System**")
+
+if "df" not in st.session_state:
+    st.warning("Please upload a dataset from the Upload page")
